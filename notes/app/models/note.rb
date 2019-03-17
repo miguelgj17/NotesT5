93c85665f938b6bdea4b1e10c771e5a4a47898b7
algoridc <algoridc@gmail.com>
2019-03-17 21:03:03 +0100

    Update note.rb
    images

--- notes/app/models/note.rb
+++ notes/app/models/note.rb
@@ -1,32 +1,32 @@
 class Note < ActiveRecord::Base
     belongs_to :user
-    aatr_accesible :photo,:titulo
-    FOTOS = File.join Rails.root, 'public', 'photo_store'
-    after_save : guardar_foto
+    aatr_accesible :image,:text
+    IMAGES = File.join Rails.root, 'public', 'image_store'
+    after_save : guardar_image
 
 
-    def photo=(file_data)
+    def image=(file_data)
 	unless file_data.blank?
 		@file_data = file_data
 		self.extension = file_data.original_filename.split('.').last.downcase
 	end
     end
     
-    def photo_filename
-	File.join FOTOS, "#{id}.#{extension}"
+    def image_filename
+	File.join IMAGES, "#{id}.#{extension}"
     end
-    def photo_path
-	"/photo_store/#{id}.#{extension}"
+    def image_path
+	"/image_store/#{id}.#{extension}"
     end
-    def has_photo?
-	File.exists? photo_filename
+    def has_pimage?
+	File.exists? image_filename
     end
     private
 
-    def guardar_foto
+    def guardar_image
 	if @file_data
-	    FileUtils.mkdir_p FOTOS
-	    File.open(photo_filename, 'wb') do |f|
+	    FileUtils.mkdir_p IMAGES
+	    File.open(image_filename, 'wb') do |f|
 		f.write(@file_data.read)
 	end
 	@file_data = nil
